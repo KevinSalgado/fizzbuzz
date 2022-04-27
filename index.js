3d8ef3f6451532ed7ace5b9aa76c9ce47ed01060
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-26 20:46:45 -0500

    auto

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Reader = require("./lib/utils/reader");
 const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
-const explorers = Reader.readJsonFile("explorers.json");
+/*const explorers = Reader.readJsonFile("explorers.json");
 //expect(explorers).not.toBeUndefined;
 
 result = ExplorerService.filterByMission(explorers, "node");
+
+console.log(result);*/
+
+const explorers = Reader.readJsonFile("explorers.json");
+//const explorer1 = { name: "Explorer1", score: 1 };
+result = FizzbuzzService.assignFizzBuzz(explorers);
 
 console.log(result);
