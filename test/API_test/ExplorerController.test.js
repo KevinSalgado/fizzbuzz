bd3b80311bc5e51e4cbbf21a35e6b204e6ce7499
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-26 23:15:12 -0500

    final

--- test/API_test/ExplorerController.test.js
+++ test/API_test/ExplorerController.test.js
@@ -1,18 +1,18 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Tests para controllers", () => {
-  test("1. test de getExplorersByMission", () => {
-    resul = ExplorerController.getExplorersByMission("node");
-    expect(resul).not.toBeUndefined;
-  });
+    test("1. test de getExplorersByMission", () => {
+        resul = ExplorerController.getExplorersByMission("node");
+        expect(resul).not.toBeUndefined;
+    });
 
-  test("2. test de getExplorersUsernamesByMission", () => {
-    resul = ExplorerController.getExplorersUsernamesByMission("node");
-    expect(resul).not.toBeUndefined;
-  });
+    test("2. test de getExplorersUsernamesByMission", () => {
+        resul = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(resul).not.toBeUndefined;
+    });
 
-  test("3 test de getExplorersAmonutByMission", () => {
-    resul = ExplorerController.getExplorersAmonutByMission("node");
-    expect(resul).toBe(10);
-  });
+    test("3 test de getExplorersAmonutByMission", () => {
+        resul = ExplorerController.getExplorersAmonutByMission("node");
+        expect(resul).toBe(10);
+    });
 });
