0c302123b2c9ec1f56064791141374096a986792
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-27 22:13:08 -0500

    run linter

--- test/API_test/ExplorerController.test.js
+++ test/API_test/ExplorerController.test.js
@@ -1,28 +1,28 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Tests para controllers", () => {
-  test("1. test de getExplorersByMission", () => {
-    resul = ExplorerController.getExplorersByMission("node");
-    expect(resul).not.toBeUndefined;
-  });
+    test("1. test de getExplorersByMission", () => {
+        resul = ExplorerController.getExplorersByMission("node");
+        expect(resul).not.toBeUndefined;
+    });
 
-  test("2. test de getExplorersUsernamesByMission", () => {
-    resul = ExplorerController.getExplorersUsernamesByMission("node");
-    expect(resul).not.toBeUndefined;
-  });
+    test("2. test de getExplorersUsernamesByMission", () => {
+        resul = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(resul).not.toBeUndefined;
+    });
 
-  test("3. test de getExplorersAmonutByMission", () => {
-    resul = ExplorerController.getExplorersAmonutByMission("node");
-    expect(resul).toBe(10);
-  });
+    test("3. test de getExplorersAmonutByMission", () => {
+        resul = ExplorerController.getExplorersAmonutByMission("node");
+        expect(resul).toBe(10);
+    });
 
-  test("4. test de getFizzBuzz", () => {
-    resul = ExplorerController.getFizzBuzz(3);
-    expect(resul).toBe("FIZZ");
-  });
+    test("4. test de getFizzBuzz", () => {
+        resul = ExplorerController.getFizzBuzz(3);
+        expect(resul).toBe("FIZZ");
+    });
 
-  test("5. test de getStackCor", () => {
-    resul = ExplorerController.getStackCor("javascript");
-    expect(resul).not.toBeUndefined;
-  });
+    test("5. test de getStackCor", () => {
+        resul = ExplorerController.getStackCor("javascript");
+        expect(resul).not.toBeUndefined;
+    });
 });
